57a6e6f32586012a6f320339bbd1a0a77a074d64
michaelahall <31089447+michaelahall@users.noreply.github.com>
2017-09-13 16:32:06 +1000

    Add files via upload

---
+++ mandelbrot.c
@@ -0,0 +1,146 @@
+// Assignment 1 Mandelbrot
+//
+// Completed by
+//  ... (z0000000)
+//  ... (z0000000)
+//
+// Modified on 2017-??-??
+// Tutor's name (dayHH-lab)
+
+// Add your own #includes here
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "mandelbrot.h"
+//#include "pixelColor.h"
+
+// Add your own #defines here
+
+#define BREAK_OUT 4
+
+// Add your own function prototypes here
+static complex complexAdd(complex a, complex b);
+static complex complexMultiply(complex a, complex b);
+static double complexAbsolute(complex c);
+static void pixelCalc (complex c);
+static double power (int base, int power);
+
+
+// Draw a single Mandelbrot tile, by calculating and colouring each of
+// the pixels in the tile.
+//
+// Your function "returns" this tile by filling in the `pixels` 2D array
+// that is passed in to your function.
+//
+// The `pixels` array is TILE_SIZE high and TILE_SIZE wide.
+// The bottom left corner of the image is pixels[0][0].
+//
+// `center` is a complex number, representing the center of the tile.
+// `z` is used to calculate the distance between each pixel in the image.
+// Each pixel is 2^(-z) apart from each other.
+void drawMandelbrot(pixel pixels[TILE_SIZE][TILE_SIZE],
+        complex center, int z) {
+    // hint: use this array to store the result from escapeGrid.
+    int grid[TILE_SIZE][TILE_SIZE];
+
+    // TODO: COMPLETE THIS FUNCTION
+
+}
+
+// Determine the number of steps required to escape the Mandelbrot set,
+// for the given complex number `c`.
+int escapeSteps(complex c) {
+
+    int steps = 0;
+    complex start = {0, 0};
+
+    // TODO: COMPLETE THIS FUNCTION
+    while (complexAbsolute(start) < BREAK_OUT && steps < MAX_STEPS) {
+        start = complexMultiply(start, start);
+        start = complexAdd(start, c);
+        steps++;
+        //printf("%d\n", steps);
+        //if (steps == MAX_STEPS) {
+        //    return NO_ESCAPE;
+       // }
+    }
+    return steps;
+
+}
+
+// Fill a grid of TILE_SIZE by TILE_SIZE pixels, with the number of
+// steps each pixel took to escape the Mandelbrot set.
+void escapeGrid(int grid[TILE_SIZE][TILE_SIZE], complex center, int z) {
+    int column;
+    int row = 0;
+    double X, Y;
+    double scale;
+    scale = 1 / power(2, z);
+    //20 + TILE_SIZE/2;
+    while (row < TILE_SIZE) {
+        int column = 0;
+        //column = 20 - TILE_SIZE/2;
+        while (column < TILE_SIZE) {
+            // centering
+            X = (0 -(TILE_SIZE / 2) + column) * scale;
+            Y = (0 - (TILE_SIZE / 2) + row) * scale;
+            // calculate current point
+            //X = X + (column * scale);
+            //Y = Y + (row * scale);
+            //printf("%d, %d\n", column, row);
+            //printf("%lf, %lf\n", scaleX, scaleY);
+            complex c = {X, Y};
+
+            grid[row][column] = escapeSteps(c);
+            printf("(%d)", grid[row][column]);
+            column++;
+        }
+        row++;
+        printf("\n");
+}
+
+}
+
+static double power (int base, int power) {
+    int i = 0;
+    double calcResult = 1;
+    while (i < power) {
+        calcResult *= base;
+        i++;
+    }
+    return calcResult;
+}
+
+
+// Add your own functions here.
+// Remember to make them static.
+static complex complexAdd(complex a, complex b){
+    // PUT YOUR CODE HERE;
+    complex value;
+    value.re = a.re + b.re;
+    value.im = a.im + b.im;
+
+    return value;
+}
+static complex complexMultiply(complex a, complex b){
+    // PUT YOUR CODE HERE;
+    complex value = {
+        .re = (a.re * b.re) - (a.im * b.im),
+        .im = (a.im * b.re) + (a.re * b.im)
+    };
+
+    return value;
+}
+static double complexAbsolute(complex c) {
+    // PUT YOUR CODE HERE;
+    double ab = (c.re * c.re + c.im * c.im);
+
+    return ab; // CHANGE THIS TO YOUR RETURN VALUE
+}
+static void pixelCalc (complex c) {
+    if (escapeSteps(c) < MAX_STEPS) {
+        printf(" ");
+    } else {
+        printf("*");
+    }
+}
---
+++ mandelbrot_test.c
@@ -0,0 +1,194 @@
+#include "mandelbrot.h"
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
+
+#define SIZE 3
+#define TERMINAL_WIDTH 80
+#define TERMINAL_HEIGHT 72
+#define MAX_ITERATIONS 256
+
+#define DRAWING_ZOOM 5
+
+void testEscapeSteps(void);
+static void pixelCalc (complex c);
+static void printAsterix (void);
+static double power(int base, int power);
+
+int main(void) {
+
+
+    //testEscapeSteps();
+    //printAsterix ();
+
+    int grid[TILE_SIZE][TILE_SIZE];
+    complex center = {20, 20};
+    //printf("%f %f", center.im, center.re);
+    escapeGrid(grid, center, 5);
+
+
+    return 0;
+}
+
+void testEscapeSteps(void) {
+
+    complex a = {0, 0};
+    assert(escapeSteps(a) == MAX_STEPS);
+
+    complex z = {-0.34, -0.0059};
+    assert(escapeSteps(z) == MAX_STEPS);
+
+    complex b = {20, 0};
+    assert(escapeSteps(b) == 1);
+
+    complex d = {3, 0};
+    assert(escapeSteps(d) == 1);
+
+    complex g = {-3, 0};
+    assert(escapeSteps(g) == 1);
+
+    complex h = {20, 20};
+    assert(escapeSteps(h) == 1);
+
+    complex lm = {-0.11, -0.74};
+    assert(escapeSteps(lm) == MAX_STEPS);
+
+
+    complex lv = {-1.5000000000000, -1.5000000000000};
+    assert (escapeSteps (lv) == 1);
+
+    complex lt = {0.3565670610700, 0.1094322227625};
+    assert (escapeSteps (lt) == 224);
+
+
+
+
+   printf (" ... escapeSteps tests passed!\n");
+    //double x = -SIZE;
+    //double y = SIZE;
+    //complex c = {x, y};
+
+    //printf(" c = %lf\n", c);
+   /* while (y > -SIZE) {
+        x = -SIZE;
+        complex c = {x, y};
+        //printf("c = %lf %lf\n", c.re, c.im);
+        while (x < SIZE) {
+            complex c = {x, y};
+            //printf("c = %lf %lf\n", c.re, c.im);
+            if (escapeSteps(c) < MAX_STEPS) {
+                printf(" ");
+                //printf("Steps = %d\n", escapeSteps(c));
+            } else {
+                printf("*");
+                //printf("Steps = %d\n", escapeSteps(c));
+            }
+            //printf("c = %lf %lf\n", c.re, c.im);
+            x++;
+        }
+        y--;
+    } */
+    /*double i = 0;
+    double j = 0;
+    while (i < SIZE) {
+        j = 0;
+        complex c = {i, j};
+        while (j < SIZE) {
+            if (escapeSteps(c) == MAX_STEPS) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+            j = j + 0.1;
+        }
+        printf("\n");
+        i = i + 0.1;
+    }  */
+
+    /*complex c = {0, 0};
+    double x, y;
+    y = c.im;
+    while (y > c.im - 2) {
+        x = c.re;
+        while (x < c.re + 3) {
+            pixelCalc(c);
+            x++;
+        }
+        printf("\n");
+        y--;
+    }*/
+
+   /*complex c = {0, 0};
+   int row = c.re;
+   int col = c.im;
+   while (row <= 255) {
+      col = 0;
+      printf("\n");
+      while (col <= 255) {
+         if (escapeSteps (c) != MAX_STEPS) {
+            printf("*");
+         } else {
+            printf(" ");
+         }
+         col++;
+      }
+      row++;
+   }*/
+   int column;
+   int row;
+   double scaleX, scaleY;
+   double scale;
+   scale = 1 / power(2, DRAWING_ZOOM);
+   row = TERMINAL_HEIGHT;
+   while (row > -TERMINAL_HEIGHT) {
+       column = -TERMINAL_WIDTH;
+            while (column < TERMINAL_WIDTH) {
+               // centering
+               scaleX = -(TERMINAL_WIDTH / 2) * scale;
+               scaleY = -(TERMINAL_HEIGHT / 2) * scale;
+               // calculate current point
+               scaleX = scaleX + (column * scale);
+               scaleY = scaleY + (row * scale);
+               //printf("%d, %d\n", column, row);
+               //printf("%lf, %lf\n", scaleX, scaleY);
+               complex c = {scaleX, scaleY};
+               if (escapeSteps(c) == MAX_STEPS) {
+                   printf("*");
+               } else {
+                   printf(" ");
+               }
+
+               column++;
+           }
+           printf("\n");
+           row--;
+
+       }
+}
+/*static void printAsterix (void) {
+   int row = 0;
+   int col = 0;
+   complex c = {row, col};
+   while (row <= 255) {
+      col = 0;
+      printf("\n");
+      while (col <= 255) {
+         if (escapeSteps (c) >= 4) {
+            printf("*");
+         } else {
+            printf(" ");
+         }
+         col++;
+      }
+      row++;
+   }
+} */
+static double power (int base, int power) {
+    int i = 0;
+    double calcResult = 1;
+    while (i < power) {
+        calcResult *= base;
+        i++;
+    }
+    return calcResult;
+}
